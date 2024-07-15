f7e8da8e67b5895432cf5daff0b590e866e88139
archibald-macharia <archibald.gacheru@student.moringaschool.com>
2024-07-15 19:17:51 +0300

    seq complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibo = []
+    if length > 0:
+            fibo.append(0) 
+    if length >= 2:
+           fibo.append(1)
+           for i in range(2, length):
+               fibo.append(fibo[-1] + fibo[-2])
+    print(fibo)
+             
+                 
+
+        
+
+print_fibonacci(9)
+    #pass
